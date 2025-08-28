25a502f0ea6a15e0f3af78b3ec31791684756372
ahdeka <ahdeka01@gmail.com>
2025-08-28 21:19:41 +0900

    [refactor] DTO의 toEntity 메서드 삭제

--- src/main/kotlin/com/back/domain/auth/dto/request/MemberSignupRequest.kt
+++ src/main/kotlin/com/back/domain/auth/dto/request/MemberSignupRequest.kt
@@ -1,8 +1,5 @@
 package com.back.domain.auth.dto.request
 
-import com.back.domain.member.entity.Member
-import com.back.domain.member.entity.Role
-import com.back.domain.member.entity.Status
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 
@@ -16,9 +13,4 @@ data class MemberSignupRequest(
 
     @field:NotBlank(message = "이름은 필수입니다")
     val name: String
-) {
-    // 현재는 Member Java Entity에 맞춰서 작성 -> 추후 코틀린 스타일로 변경 필요
-    fun toEntity(): Member {
-        return Member(email, password, name, null, Role.USER, Status.ACTIVE)
-    }
-}
+){}
