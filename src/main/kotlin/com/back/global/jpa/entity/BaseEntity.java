90b67d4b5c9ee686507e2d3f1f89cfe63da7bac3
rladbstn0 <140579355+rladbstn0@users.noreply.github.com>
2025-08-28 14:26:14 +0900

    [refactor] BaseEntity @Getter 제거 후 직접 구현 (#26)

--- src/main/kotlin/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/kotlin/com/back/global/jpa/entity/BaseEntity.java
@@ -1,7 +1,6 @@
 package com.back.global.jpa.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,7 +11,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Getter
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // mysql은 반드시 GenerationType을 사용하여 AutoIncrement합니다.
@@ -23,4 +21,16 @@ public abstract class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
 }
