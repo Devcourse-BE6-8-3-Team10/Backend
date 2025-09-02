f356fc64b21a41f21b679867b6a916fe295b9426
younsoo <younsoo6103@naver.com>
2025-09-02 14:42:08 +0900

    [refactor] BackApplication Kotlin 변환

---
+++ src/test/kotlin/com/back/BackApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class BackApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
